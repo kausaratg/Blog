9c5f0f08250d08ea9a3504b3e1f9bedd8f5e651a
kassy <kausaratg@gmail.com>
2022-09-28 23:39:23 +0100

    created basic Crud Fuction with ApiView

--- api_blog/views.py
+++ api_blog/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.shortcuts import get_object_or_404, render
+from rest_framework.views import APIView
 from api_blog.serializers import BlogAPI
 from api_blog.models import Blog
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
-class BlogCreateAPIView(ListCreateAPIView):
-    serializer_class = BlogAPI
-    def get_queryset(self):
-        return Blog.objects.all()
+class BlogCreateAPIView(APIView):
+    def get(self, request):
+        blog = Blog.objects.all()
+        serializer = BlogAPI(blog, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        serializer = BlogAPI(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class BlogUpdateAPIView(RetrieveUpdateDestroyAPIView):
-    serializer_class = BlogAPI
+class BlogUpdateAPIView(APIView):
 
-    def get_queryset(self):
-        return Blog.objects.all()
+    def get(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk)
+        serializer = BlogAPI(blog)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk)
+        serializer = BlogAPI(blog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        blog = get_object_or_404(Blog, pk=pk)
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
